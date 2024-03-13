9ae2028cda3be067cfe812d9456fa9ed749fd6c5
Stellar-Stellar <86974310+Stellar-Stellar@users.noreply.github.com>
2024-03-12 21:07:46 -0500

    added display loop and a book image

--- code.js
+++ code.js
@@ -8,13 +8,26 @@ function Book(title, author, pages, reading_status){
     this.pages = pages;
     this.reading_status = reading_status
     this.info = function(){
-        return(this.title + ", " + this.author + ", " + this.pages + ", " + this.reading_status)
+        return(title + ", " + author + ", " + pages + ", " + reading_status)
     };
 }
 
-function addBookToLibrary() {
-    myLibrary.push(theHobbit)
+//Pushes the book's information into the myLibrary Array
+function addBookToLibrary(input) {
+    myLibrary.push(input.info())
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet")
-console.log(theHobbit.info())
+function displayBooks(){
+    for(i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i])
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+const theLastWish = new Book("The Last Wish", "Andrzej Sapkowski", "288 pages", "finished reading");
+const theCallofCthulu = new Book("The Call of Cthulu and Other Weird Stories", "H.P Lovecraft", "420 pages", "not read yet");
+addBookToLibrary(theHobbit);
+addBookToLibrary(theLastWish);
+addBookToLibrary(theCallofCthulu);
+displayBooks();
+
