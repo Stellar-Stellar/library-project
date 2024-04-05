9aa85485c80e4a2be20890bf80f5ee341d97856e
Stellar-Stellar <86974310+Stellar-Stellar@users.noreply.github.com>
2024-04-04 23:33:48 -0500

    added functionality to submit button

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 
-
+let reading_stat;
 const myLibrary = [];
+
+const submit_btn = document.querySelector("#submit-button")
 
 function Book(title, author, pages, reading_status){
     this.title = title;
@@ -12,15 +14,37 @@ function Book(title, author, pages, reading_status){
     };
 }
 
+
 //Pushes the book's information into the myLibrary Array
 function addBookToLibrary(input) {
     myLibrary.push(input.info())
 }
 
+//Displays the entire array of books
 function displayBooks(){
     for(i = 0; i < myLibrary.length; i++) {
         console.log(myLibrary[i])
+        return(myLibrary[i])
     }
+}
+
+submit_btn.addEventListener('click', () => {
+    createCard();
+})
+
+function createCard() {
+    const newCard = document.createElement("div");
+    newCard.setAttribute("id", "book-card")
+
+    const newImage = document.createElement("img");
+    newImage.setAttribute("id", "book-image")
+
+    const newTitle = document.createElement("div");
+    newTitle = setAttribute("id", "book-title")
+    const cardContent = document.createTextNode(myLibrary[0])
+    newCard.appendChild(cardContent)
+    const formID = document.getElementById("form-container")
+    document.body.insertBefore(newCard, formID)
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
